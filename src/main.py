7d9e3e35d8f7c9f0813adffa34268a9ddee1ae70
GAKUGAKUGAKU <gaku5946@gmail.com>
2020-04-11 03:31:34 +0900

    test_commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+def test():
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        arg1 (int): Description of arg1
+
+    Returns:
+        bool: Description of return value
+    """
+    return True
