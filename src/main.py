a77ce63926dc5b1f06ca7b10533dcd42eef925e6
GAKUGAKUGAKU <gaku5946@gmail.com>
2020-04-12 01:28:15 +0900

    init

--- src/main.py
+++ src/main.py
@@ -1,12 +1,41 @@
-def test():
-    """Summary line.
+# -*- coding: utf-8 -*-
+import subprocess
+import socket 
 
-    Extended description of function.
+class Ping(object):
+    def __init__(self, hosts):
+        for host in hosts:
+            # pingコマンド
+            ping = subprocess.Popen(["ping", "-c", "1", host],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
 
-    Args:
-        arg1 (int): Description of arg1
+            # ping試験
+            out, error = ping.communicate()
 
-    Returns:
-        bool: Description of return value
-    """
-    return True
+            # 接続できなかったら
+            if error:
+                print('[NG]: ' +  host + ', Msg->\'' + error.rstrip() + '\'')
+
+            # 接続できたら
+            else:
+                print('[OK]: ' +  host)
+                print(ping.stdout)
+                print(ping.stderr)
+
+class Tcp(object):
+    def __init__(self, hosts):
+        for host in hosts:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            result = sock.connect_ex((host, 80))
+            if result == 0:
+                print('[OK]: ' +  host)
+            else:
+                print('[NG]: ' +  host)
+
+# ping試験するホスト
+hosts = ['www.google.com', 'algorithm.joho.info',]
+
+# ping試験
+Tcp(hosts)
